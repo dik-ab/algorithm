677f06a09eac26f78d2e6f9757e94f1ade4db545
nopia1 <abedaiki06@gmail.com>
2023-05-14 11:29:07 +0900

    binary search

--- tessoku_book/template.cpp
+++ tessoku_book/template.cpp
@@ -2,7 +2,11 @@
 #include <vector>
 #include<algorithm>
 using namespace std;
+typedef long long ll;
+#define rep0(i,n) for(int i=0;i<n;i++)
+#define rep(i,n) for(int i=1;i<=n;i++)
 
 int main(){
+
     return 0;
 }
