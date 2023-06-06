6903ff4f71bd498b92690c9431d282a398c12981
dik-ab <abedaiki06@gmail.com>
2023-06-06 22:47:29 +0900

    add

--- abc304/a.cpp
+++ abc304/a.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#include <"atcoder/all">
+#include "atcoder/all"
 using namespace std;
 using mint = atcoder::modint998244353;
 typedef long long ll;
@@ -10,9 +10,21 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 //ll mod=2147483647;
 //ll mod=1000000007
 constexpr long long INF = 1000000000000000000;
+int n;
+vector<string>s;
+vector<int>a;
 
 int main()
 {
-
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>s[i]>>a[i];
+    }
+    pair<int,int>m={a[0],0};
+    for(int i=1;i<n;i++)m=min(m,{a[i],i});
+    int p=m.second;
+    for(int i=0;i<n;i++){
+        cout<<s[(p+i)%n]<<endl;
+    }
     return 0;
 }
