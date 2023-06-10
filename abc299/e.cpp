1e16c98f4d81ffd22c7bdfcf7d15e9cdb44bb591
dik-ab <abedaiki06@gmail.com>
2023-06-10 21:41:23 +0900

    add

---
+++ abc299/e.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using mint = atcoder::modint998244353;
+typedef long long ll;
+#define rep0(i,n) for(int i=0;i<=n;i++)
+#define rep(i,n) for(int i=1;i<=n;i++)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+//ll mod=2147483647;
+//ll mod=1000000007
+constexpr long long INF = 1000000000000000000;
+
+int main()
+{
+    
+    return 0;
+}
