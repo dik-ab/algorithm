bd0ec53bac07150b229bf0b7f66fbc0f221b23ca
dik-ab <abedaiki06@gmail.com>
2023-08-24 19:32:22 +0900

    add

--- tessoku_book/a74.cpp
+++ tessoku_book/a74.cpp
@@ -13,7 +13,7 @@ int main(){
             if(p[i][j]!=0)x[i]=p[i][j];y[j]=p[i][j];
         }
     }
-    for(int i=1;i<=n;i++){
+    for(int i=13i<=n;i++){
         for(int j=i+1;j<=n;j++){
             if(x[i]>x[j])inversionx+=1;
             if(y[i]>y[j])inversiony+=1;
